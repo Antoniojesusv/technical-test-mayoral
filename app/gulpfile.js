a33b083eeb385016a5528894629ccce98daf6475
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2020-01-16 13:07:09 +0100

    build: added gulp file

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,10 +1,18 @@
 const gulp = require("gulp");
 
-gulp.task("default", function() {
+gulp.task("css", function() {
   return gulp
     .src([
       "./node_modules/normalize.css/normalize.css",
       "./node_modules/bootstrap/dist/css/bootstrap.css"
     ])
-    .pipe(gulp.dest("./assets"));
+    .pipe(gulp.dest("./assets/css"));
 });
+
+gulp.task("javascript", () => {
+  return gulp
+    .src(["./node_modules/jquery/dist/jquery.min.js", "./controllers/main.js"])
+    .pipe(gulp.dest("./assets/js"));
+});
+
+gulp.task("default", gulp.series("css", "javascript"));
